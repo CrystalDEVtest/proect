afbfc9d6e81662a7b202e4774a2d2a77b940e86d
CrystalDEVtest <161251881+CrystalDEVtest@users.noreply.github.com>
2024-02-29 21:27:00 +0300

    obnova133432

---
+++ no.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Загрузка файлов</title>
+</head>
+<body>
+	<h1>Загрузка файлов</h1><br>
+	<hr>
+	
+	 <a href='https://disk.yandex.ru/d/pOqrEZN62xqStg' download=""><button>Скачать FILES</button></a>
+</body>
+</html>
